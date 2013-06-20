04e3fe919152a3c8f0b2b9894a203b6c53e2049f
Jan Mentzel <jan@islandofwaiting.de>
2013-06-20 16:04:54 +0200

    added Hypercharge::Schema::each(&proc)

--- lib/ruby/hypercharge/schema.rb
+++ lib/ruby/hypercharge/schema.rb
@@ -6,18 +6,26 @@ require "hypercharge/schema/fixture"
 
 
 module Hypercharge
-  module Schema
+	module Schema
 
-	  # validates a hash with JSON-Schema
-	  def self.validate(type, data)
-	    JSON::Validator.fully_validate(schema_path_for(type), data)
-	  end
+		# validates a hash with JSON-Schema
+		def self.validate(type, data)
+			JSON::Validator.fully_validate(schema_path_for(type), data)
+		end
 
-	  # returns the path for a JSON-Schema
-	  # @param [Hypercharge::Payment::Type, Hypercharge::PaymentTransaction::Type] type  the type
-	  # @return [String] path
-	  def self.schema_path_for(type)
-	    File.expand_path("../../../../json/#{type}.json", __FILE__)
-	  end
-  end
+		# returns the path for a JSON-Schema
+		# @param [Hypercharge::Payment::Type, Hypercharge::PaymentTransaction::Type] type  the type
+		# @return [String] path
+		def self.schema_path_for(type)
+			File.expand_path("../../../../json/#{type}.json", __FILE__)
+		end
+
+		# yields with json schema files full path.
+		# If you need the schema as hash, please feel free to IO.read() and JSON.parse().
+		def self.each
+			Dir[File.expand_path("../../../../json/*.json", __FILE__)].each do |path|
+				yield path
+			end
+		end
+	end
 end
